689606633fb465770d9e18a2c7fa421d3e6ff58a
mohamedseif-10 <mohamedseif.a1@gmail.com>
2025-12-03 04:35:58 +0200

    Feature Engineering(comp features)

--- utils/FeatureEngineeringFunctions.py
+++ utils/FeatureEngineeringFunctions.py
@@ -4,40 +4,34 @@ import numpy as np
 
 def TransformDateFeature(data):
     """Extracts date features from the 'Date' column in the DataFrame."""
-    data["Year"] = data["Date"].dt.year
-    data["Month"] = data["Date"].dt.month
-    data["Day"] = data["Date"].dt.day
-
-    data["DayOfYear"] = data["Date"].dt.dayofyear
-    data["WeekOfYear"] = data["Date"].dt.isocalendar().week.astype(int)
-
-    # Sales often spike at the beginning and end of the month.
-    # This flag captures the last few days of the month.
+    data["Year"] = data["Date"].dt.year.astype(np.int16)
+    data["Month"] = data["Date"].dt.month.astype(np.int8)
+    data["Day"] = data["Date"].dt.day.astype(np.int8)
+    
+    data["DayOfYear"] = data["Date"].dt.dayofyear.astype(np.int16)
+    data["WeekOfYear"] = data["Date"].dt.isocalendar().week.astype(np.int8)
     data["IsLastDayOfMonth"] = data["Date"].dt.is_month_end.astype(bool)
-    import numpy as np
-
-    # 1. Cyclical Encoding for DayOfWeek -- it's important to capture the cyclical nature of days in a week
-    # NN may misinterpret Monday (1) and Sunday (7) as being far apart numerically the same as 1-12 for months
+    
+    # Cyclical features
     data["DayOfWeek_sin"] = np.sin(2 * np.pi * data["DayOfWeek"] / 7).astype(np.float32)
     data["DayOfWeek_cos"] = np.cos(2 * np.pi * data["DayOfWeek"] / 7).astype(np.float32)
-
     data["Month_sin"] = np.sin(2 * np.pi * data["Month"] / 12).astype(np.float32)
     data["Month_cos"] = np.cos(2 * np.pi * data["Month"] / 12).astype(np.float32)
-
+    
     data["IsWeekend"] = data["DayOfWeek"].isin([6, 7]).astype(bool)
     data["IsMonthEnd"] = data["Date"].dt.is_month_end.astype(bool)
     data["IsMonthStart"] = data["Date"].dt.is_month_start.astype(bool)
-
-    data.drop("Date", axis=1, inplace=True)
     return data
 
 
-def clean_unseen_test_data(test_path: str):
+def loadAndPrepareTest(test_path: str):
+    """Cleans and preprocesses the unseen test data."""
+    
     test_data_types = {
         "Store": "int16",
         "DayOfWeek": "int8",
         "Open": "float32",
-        "Promo": "int8",
+        "Promo": "bool",
         "SchoolHoliday": "int8",
         "StateHoliday": "category",
         "StoreType": "category",
@@ -68,6 +62,27 @@ def clean_unseen_test_data(test_path: str):
     test_data["CompetitionOpenSinceMonth"] = test_data["CompetitionOpenSinceMonth"].fillna(1)
     test_data["CompetitionDistanceMissing"] = test_data["CompetitionDistanceMissing"].astype("bool")
     test_data["CompetitionOpenMissing"] = test_data["CompetitionOpenMissing"].astype("bool")
+    test_data.drop(columns=["Open"], axis=1, inplace=True)
+    test_data = TransformDateFeature(test_data)
+    test_data = feature_competition_duration(test_data)
     return test_data
 
-test_data = clean_unseen_test_data("../Data/Preprocessed_data/merged_data_before_preprocessing/merged_testing_data.csv")
+
+def feature_competition_duration(data):
+    """Creates 'OpenDuration' feature based on competition open date and current date."""
+    data["OpenDate"] = pd.to_datetime(
+        data["CompetitionOpenSinceYear"].astype(int).astype(str)
+        + "-"
+        + data["CompetitionOpenSinceMonth"].astype(int).astype(str)
+        + "-01",
+        errors="coerce")
+    
+    data["OpenDuration"] = (
+    (data["Date"].dt.year - data["OpenDate"].dt.year) * 12
+    + (data["Date"].dt.month - data["OpenDate"].dt.month)).astype(np.int16)
+    
+    data["OpenDuration"] = data["OpenDuration"].apply(
+        lambda x: 24 if x > 24 else (0 if x < 0 else x)).astype(np.int8)
+    data.drop(columns=["OpenDate", 'CompetitionOpenSinceYear', 'CompetitionOpenSinceMonth', 'Date'], axis=1, inplace=True)
+    
+    return data
